8904582acee71f8f7ff27fb02e55e91ab78d5431
Josiah Gore <josiah.gore@veeva.com>
2023-07-04 01:21:48 -0400

    add flowbite-vue for components

--- src/components/ClickeyButton.tsx
+++ src/components/ClickeyButton.tsx
@@ -1,5 +1,6 @@
 import { defineComponent } from 'vue';
 import type { PropType } from 'vue';
+import { Button } from 'flowbite-vue';
 
 export default defineComponent({
     name: 'ClickeyButton',
@@ -18,12 +19,9 @@ export default defineComponent({
         return () => (
             <div class="flex flex-col">
                 <h1 class="mb-6">{props.instructions}</h1>
-                <button
-                    class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
-                    onClick={props.onClick}
-                >
+                <Button size="xl" pill={true} gradient="pink-orange" onClick={props.onClick}>
                     click!
-                </button>
+                </Button>
             </div>
         );
     },
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
+import 'flowbite/plugin';
+
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ['./index.html', './src/App.vue', './src/{pages,components}/*.{vue,js,ts,jsx,tsx}'],
+    content: [
+        './index.html',
+        './src/App.vue',
+        './src/{pages,components}/*.{vue,js,ts,jsx,tsx}',
+        'node_modules/flowbite/**/*.{js,jsx,ts,tsx}',
+        'node_modules/flowbite-vue/**/*.{js,jsx,ts,tsx}',
+    ],
     // https://tailwindcss.com/docs/theme
     theme: {
         extend: {},
     },
-    plugins: [],
+    plugins: ['flowbite/plugin'],
 };
